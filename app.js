ff31e9b64d7fa8c6c965aa6d99cff9dca1f44026
GuillaumeVanneste <guillaumevanneste@hotmail.fr>
2018-08-08 12:04:14 +0200

    :tata: First commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+
+app.use(express.static(__dirname + '/client/'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/client/index.html')
+})
+
+server.listen(process.env.PORT || 2000)
